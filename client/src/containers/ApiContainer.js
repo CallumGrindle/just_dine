8456245451fa7cb2224d104c0d6d89e2892c28d1
CallumGrindle <callumgrindle@gmail.com>
2019-11-01 20:09:40 +0000

    getting coords on start

--- client/src/containers/ApiContainer.js
+++ client/src/containers/ApiContainer.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
-import {GoogleKey} from '../keys.js'
+import {ZomatoKey} from '../keys.js'
 
 class ApiContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      coords: null
     }
   }
 
   componentDidMount() {
     // const place_id = null
-    const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${GoogleKey}`
+    const url = ``
     fetch(url)
       .then(res => res.json())
       .then(data => this.state({ data: data }))
       .catch(err => console.error(err))
+
+    let lat;
+    let lon;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        this.setState({latlon: {lat: lat, lon: lon}});
+      })
+    }
+
   }
 
   render() {
