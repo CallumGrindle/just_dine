28efd1b1e2011bbb16316deb8f10021cd69a4774
PammyLo <pamela@tuminds.com>
2019-11-04 11:57:30 +0000

    List rendering nicely, needs css

--- client/src/components/RestaurantListItem.js
+++ client/src/components/RestaurantListItem.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import RestaurantDetail from './RestaurantDetail';
 
-const RestaurantListItem = ({ name, cuisine, location, price_range, price_level }) => {
-
-
+const RestaurantListItem = ({
+  name, cuisine, location, price_range, price_level }) => {
 
   return (
     <div className="restaurant-list-item">
       <h3> { name } </h3>
       <p>Cuisine: { cuisine }</p>
       <p>Location: { location }</p>
-      <p>Price Range: { price_range },  Average Cost for Two: £ { price_level }</p>
-
-
-    <RestaurantDetail />
+      <p>Price Range: { price_range },
+      Average Cost for Two: £ { price_level }</p>
     </div>
   );
 };
