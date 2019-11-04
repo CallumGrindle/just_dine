afab89010b45c7610a9e221552bb25f27f67a312
Michael Crockett <mike.crockett76@gmail.com>
2019-11-04 14:38:50 +0000

    Further CSS styling

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './AppHeader.css'
+import logo from '../images/colours_160.png'
 
 const AppHeader = (props) => {
 
   return (
     <header id="page-header">
-      <section>Section 1</section>
-      <section>Section 2</section>
-      <section>Section 3</section>
+      <section id="logo"><img src={logo} alt="Just Dine Logo"></img>
+<h1>Why stay in when you can go out?</h1></section>
+      <section>Search component</section>
+      <section>Favourites button</section>
     </header>
   )
 }
