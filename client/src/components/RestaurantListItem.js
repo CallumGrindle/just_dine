3aaf72c48f3e93c74a003f1286996bacf1f621d5
JAtherton <atherton374@btinternet.com>
2019-11-04 09:39:16 +0000

    Components added

---
+++ client/src/components/RestaurantListItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RestaurantListItem = (props) => {
+
+
+
+  return (
+    <div className="restaurant-list-item">
+      <h2> Restaurant </h2>
+    </div>
+  );
+};
+
+export default RestaurantListItem;
