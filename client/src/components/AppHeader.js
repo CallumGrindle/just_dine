1386514deaf0b2bedc6f095917fab99c423ddddc
Michael Crockett <mike.crockett76@gmail.com>
2019-11-04 20:43:47 +0000

    css adjustment

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -7,7 +7,7 @@ const AppHeader = (props) => {
   return (
     <header id="page-header">
       <section id="logo"><img src={logo} alt="Just Dine Logo"></img>
-<h1>Why stay in when you can dine out?</h1></section>
+<h1>Why eat in when you can dine out?</h1></section>
       <section id="search-box"><h2>Search for a local restaurant</h2>
       <select></select></section>
       <section id="favourites-box"><h2>Take me to my favourite restaurants</h2></section>
