e668bdae80473af54409d5f204557483f80c414c
CallumGrindle <callumgrindle@gmail.com>
2019-11-05 15:14:17 +0000

    component setup

---
+++ client/src/components/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Filter = (props) => {
+  return (
+    <h1>Filter</h1>
+  )
+
+}
+
+export default Filter
--- client/src/containers/ControlsContainer.js
+++ client/src/containers/ControlsContainer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Search from '../components/Search.js';
+import Filter from '../components/Filter.js'
 
 const ControlsContainer = (props) => {
 
   return (
-    <Search
-    onSearchChange={props.onSearchChange}
-    searchTerm={props.searchTerm}
-    onSearchSubmit={props.onSearchSubmit}/>
+    <div>
+      <Search
+      onSearchChange={props.onSearchChange}
+      searchTerm={props.searchTerm}
+      onSearchSubmit={props.onSearchSubmit}/>
+      <Filter />
+    </div>
   )
 }
 
