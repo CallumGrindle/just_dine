902ac372311780268924ab42ee67141448c7114f
JAtherton <atherton374@btinternet.com>
2019-11-05 10:18:47 +0000

    details added

--- client/src/components/RestaurantDetail.js
+++ client/src/components/RestaurantDetail.js
@@ -3,7 +3,18 @@ import React from 'react';
 const RestaurantDetail = ({selectedRestaurant}) => {
   if (!selectedRestaurant) return null;
   return (
-    <h1>{selectedRestaurant.restaurant.name}</h1>
+    <div className="restaurant-detail">
+      <h1> {selectedRestaurant.restaurant.name} </h1>
+      <p> {selectedRestaurant.restaurant.location.address} </p>
+      <p>Tel: {selectedRestaurant.restaurant.phone_numbers} </p>
+      <p>Hours: {selectedRestaurant.restaurant.timings} </p>
+    
+
+
+    </div>
+
+
+
   )
 }
 
