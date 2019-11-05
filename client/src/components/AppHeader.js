bc8feeff4b2203e9f0555251082e1002ca7e526a
Michael Crockett <mike.crockett76@gmail.com>
2019-11-05 13:25:26 +0000

    Detail box added and styled

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -9,7 +9,7 @@ const AppHeader = (props) => {
     <header id="page-header">
       <section id="logo"><img src={logo} alt="Just Dine Logo"></img>
         <h1>Why eat in when you can dine out?</h1></section>
-      <section id="search-box"><h2>Search for a local restaurant</h2>
+      <section id="search-box"><h2>Search for a restaurant</h2>
       <ControlsContainer
       onSearchChange={ props.onSearchChange }
       onSearchSubmit={ props.onSearchSubmit }
--- client/src/components/RestaurantDetail.js
+++ client/src/components/RestaurantDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './RestaurantDetail.css'
 
 const RestaurantDetail = ({
   selectedRestaurant, selectedFavourite, markFav, favourite }) => {
     if ((!selectedRestaurant) && (!selectedFavourite)) return null;
     return (
-      <div>
+      <div className="restaurant-detail">
         <h1>{selectedRestaurant.restaurant.name}</h1>
         <p> {selectedRestaurant.restaurant.location.address} </p>
         <p>Tel: {selectedRestaurant.restaurant.phone_numbers} </p>
