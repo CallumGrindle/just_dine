fd624e33ff0fd6dbd6ef841356703fe1a01f2daa
Michael Crockett <mike.crockett76@gmail.com>
2019-11-06 11:53:07 +0000

    Detail styling

--- client/src/components/RestaurantDetail.js
+++ client/src/components/RestaurantDetail.js
@@ -21,7 +21,7 @@ const RestaurantDetail = ({
     return (
       <div className="restaurant-detail">
         <h1>{selectedRestaurant.restaurant.name}</h1>
-        <p> {selectedRestaurant.restaurant.location.locality} </p>
+        <p> Location: {selectedRestaurant.restaurant.location.locality} </p>
         <p> {selectedRestaurant.restaurant.location.address} </p>
         <p>Hours: {selectedRestaurant.restaurant.timings} </p>
         <p>Tel: {selectedRestaurant.restaurant.phone_numbers} </p>
@@ -29,14 +29,17 @@ const RestaurantDetail = ({
         <a href={selectedRestaurant.restaurant.menu_url}>View Menu</a>
         <a href="mailto:booking@restaurant.com?Subject=Request Booking%20" target="_top">Request Booking</a>
 
-        <section>
-        <h2>{favourite ? '' : 'add to my favourites'}</h2>
+        <section id="fav-section">
+        <h2>{favourite ? '' : 'Add to my favourites'}</h2>
         <HeartCheckbox
           checked={ favourite }
           onClick={ () => checkboxFav(selectedRestaurant) } />
         </section>
 
+        <section id="reviews">
+        <h2>Reviews</h2>
         <ul> { reviewNodes } </ul>
+        </section>
 
       </div>
     )
