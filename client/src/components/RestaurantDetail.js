fe85a6977de355f40280502584246ca053a07e8c
JAtherton <atherton374@btinternet.com>
2019-11-06 11:52:03 +0000

    price details added

--- client/src/components/RestaurantDetail.js
+++ client/src/components/RestaurantDetail.js
@@ -5,6 +5,18 @@ import HeartCheckbox from 'react-heart-checkbox';
 
 const RestaurantDetail = ({
   selectedRestaurant, favourite, checkboxFav }) => {
+
+    function priceDisplay() {
+      let string ="";
+      for (let count=0; count<selectedRestaurant.restaurant.price_range; count++) {
+        string+=selectedRestaurant.restaurant.currency||'*'
+      }
+      return string
+    }
+
+
+
+
     if (!selectedRestaurant) return null;
 
     const reviewNodes = selectedRestaurant.restaurant.all_reviews.reviews
@@ -16,15 +28,27 @@ const RestaurantDetail = ({
             index={ index }
           />
         );
+
+
+
+
+
+
+
+
+
       });
 
     return (
       <div className="restaurant-detail">
         <h1>{selectedRestaurant.restaurant.name}</h1>
+        <p>{selectedRestaurant.restaurant.cuisines} </p>
         <p> {selectedRestaurant.restaurant.location.locality} </p>
         <p> {selectedRestaurant.restaurant.location.address} </p>
         <p>Hours: {selectedRestaurant.restaurant.timings} </p>
         <p>Tel: {selectedRestaurant.restaurant.phone_numbers} </p>
+        <p>Price Range: { priceDisplay() } </p>
+        <p>Average Cost for 2: { selectedRestaurant.restaurant.currency }{ selectedRestaurant.restaurant.average_cost_for_two }</p>
         <img src={selectedRestaurant.restaurant.thumb} />
         <a href={selectedRestaurant.restaurant.menu_url}>View Menu</a>
         <a href="mailto:booking@restaurant.com?Subject=Request Booking%20" target="_top">Request Booking</a>
